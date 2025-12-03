5435a383606a38e2f8805e117823b4a8110f16bb
MaurasBraj <mauritz.almgren@gmail.com>
2025-12-03 12:46:09 +0100

    Added save and default button, new parameters, new cities and some functionality to the graph depending on the parameter.

--- project/project.ino
+++ project/project.ino
@@ -9,12 +9,14 @@
 #include <lvgl.h>
 #include <vector>
 #include <math.h>
+#include <Preferences.h>
 
 std::vector<int> fetchNoonData(String str, double lat, double lon);
 std::vector<int> fetchPastData(int data, int station);
 
 static void create_temperature_chart(const std::vector<int>& data);
 static void create_forecast_table(const std::vector<int>& temps);
+static void create_settings_button();
 
 void printVector(std::vector<int> vector){
   for (int i : vector){
@@ -24,8 +26,8 @@ void printVector(std::vector<int> vector){
 }
 
 // Wi-Fi credentials (Delete these before commiting to GitHub)
-static const char* WIFI_SSID     = "BTH_Guest";
-static const char* WIFI_PASSWORD = "oliv95lila";
+static const char* WIFI_SSID     = "Agartha Entry Point";
+static const char* WIFI_PASSWORD = "mackan12";
 
 LilyGo_Class amoled;
 
@@ -45,14 +47,34 @@ static lv_obj_t* t3_content;
 const double LAT = 56.2;
 const double LON = 15.5869;
 
-// Strings for the selected city in settings
-String selectedCity = "Karlskrona";
+// Last tile before the switch to settings
+int currentTile = 0;
 
+// Global variables
 
-// Strings for the selected parameter in settings
-String selectedParam = "Temperature";
+String selectedParam;
+String selectedCity;
+String str;
 
-String str = "air_temperature";
+Preferences prefs;
+// Saves the selected parameters so that they are saved even after restart.
+void saveDefaults() {
+  prefs.begin("weather", false);
+  prefs.putString("city", selectedCity);
+  prefs.putString("param", selectedParam);
+  prefs.putString("str", str);
+  prefs.end();
+}
+// Loads the selected parameters after a restart and when going to press the default button.
+void loadDefaults() {
+  prefs.begin("weather", true);
+  selectedCity = prefs.getString("city", "Karlskrona");
+  selectedParam = prefs.getString("param", "Temperature");
+  str = prefs.getString("str", "air_temperature");
+  prefs.end();
+
+  updateWeatherUI();
+}
 
 enum Karlskrona{
   Station = 65090,
@@ -72,23 +94,31 @@ void updateWeatherUI() {
   if(selectedCity == "Karlskrona"){
     lat = 56.160820; lon = 15.586710; station = 65090;
   } else if(selectedCity == "Stockholm"){
-    lat = 59.329323; lon = 18.068581; station = 98230;
+    lat = 59.329323; lon = 18.068581; station = 97400;
   } else if(selectedCity == "Goteborg"){
-    lat = 57.708870; lon = 11.974560; station = 71420;
+    lat = 57.708870; lon = 11.974560; station = 72420;
+  } else if(selectedCity == "Malmo"){
+    lat = 55.604980; lon = 13.003822; station = 53300;
+  } else if(selectedCity == "Kiruna"){
+    lat = 67.855797; lon = 20.225283; station = 180940;
   }
 
   if(selectedParam == "Temperature"){
     // Code for temperature in the API
-    param = 2;
+    param = 1;
     str = "air_temperature";
-  } else if (selectedParam == "Rain"){
-    // Code for rain amount in the API
-    param = 5;
-    str = "precipitation_amount_mean";
+  } else if (selectedParam == "Humidity"){
+    // Code for humidity in the API
+    param = 6;
+    str = "relative_humidity";
   } else if (selectedParam == "Wind_speed"){
     // Code for Wind_speed in the API
     param = 4;
     str = "wind_speed";
+    // Code for air pressure
+  } else if (selectedParam == "Air_pressure"){
+    param = 9;
+    str = "air_pressure_at_mean_sea_level";
   }
 
   // Fetch data
@@ -163,6 +193,77 @@ static void on_dd_par_clicked(lv_event_t* e)
   }
 }
 
+static void create_settings_button()
+{
+  lv_obj_t* label;
+
+  // Settings button on Tile 2
+  lv_obj_t* setting_button1 = lv_btn_create(t2);
+  lv_obj_add_event_cb(setting_button1, [](lv_event_t* e){
+      if (lv_event_get_code(e) == LV_EVENT_CLICKED) {
+          currentTile = 1; // we came from tile 2
+          lv_obj_scroll_to_view(t4, LV_ANIM_ON); // go to settings
+      }
+  }, LV_EVENT_ALL, NULL);
+  lv_obj_align(setting_button1, LV_ALIGN_TOP_RIGHT, 0, 0);
+  label = lv_label_create(setting_button1);
+  lv_label_set_text(label, "Settings");
+  lv_obj_center(label);
+
+  // Settings button on Tile 3
+  lv_obj_t* setting_button2 = lv_btn_create(t3);
+  lv_obj_add_event_cb(setting_button2, [](lv_event_t* e){
+      if (lv_event_get_code(e) == LV_EVENT_CLICKED) {
+          currentTile = 2; // we came from tile 3
+          lv_obj_scroll_to_view(t4, LV_ANIM_ON); // go to settings
+      }
+  }, LV_EVENT_ALL, NULL);
+  lv_obj_align(setting_button2, LV_ALIGN_TOP_RIGHT, 0, 0);
+  label = lv_label_create(setting_button2);
+  lv_label_set_text(label, "Settings");
+  lv_obj_center(label);
+
+  // Back button on Settings tile
+  lv_obj_t* back_btn = lv_btn_create(t4);
+  lv_obj_align(back_btn, LV_ALIGN_TOP_RIGHT, 0, 0);
+  lv_obj_add_event_cb(back_btn, [](lv_event_t* e){
+      if (lv_event_get_code(e) == LV_EVENT_CLICKED) {
+          if (currentTile == 1) lv_obj_scroll_to_view(t2, LV_ANIM_ON);
+          else if (currentTile == 2) lv_obj_scroll_to_view(t3, LV_ANIM_ON);
+          else lv_obj_scroll_to_view(t1, LV_ANIM_ON); // fallback
+      }
+  }, LV_EVENT_ALL, NULL);
+
+  lv_obj_t* back_label = lv_label_create(back_btn);
+  lv_label_set_text(back_label, "Back");
+  lv_obj_center(back_label);
+}
+
+static void create_save_buttons()
+{
+  lv_obj_t* default_button = lv_btn_create(t4);
+  lv_obj_align(default_button, LV_ALIGN_CENTER, 0, 40);
+  lv_obj_add_event_cb(default_button, [](lv_event_t* e){
+    if (lv_event_get_code(e) == LV_EVENT_CLICKED) {
+      loadDefaults();
+    }
+  }, LV_EVENT_ALL, NULL);
+  lv_obj_t* default_label = lv_label_create(default_button);
+  lv_label_set_text(default_label, "Default");
+  lv_obj_center(default_label);
+
+  lv_obj_t* save_button = lv_btn_create(t4);
+  lv_obj_align(save_button, LV_ALIGN_CENTER, 0, 80);
+  lv_obj_add_event_cb(save_button, [](lv_event_t* e){
+    if (lv_event_get_code(e) == LV_EVENT_CLICKED) {
+      saveDefaults();
+    }
+  }, LV_EVENT_ALL, NULL);
+  lv_obj_t* save_label = lv_label_create(save_button);
+  lv_label_set_text(save_label, "Save");
+  lv_obj_center(save_label);
+
+}
 
 // Function: Create chart for tile 3 (Used for testing fuctionality)
 static void create_temperature_chart(const std::vector<int>& data)
@@ -174,14 +275,22 @@ static void create_temperature_chart(const std::vector<int>& data)
 
   // Create chart object on tile 3
   lv_obj_t* chart = lv_chart_create(t3_content);
-  lv_obj_set_size(chart, lv_disp_get_hor_res(NULL) - 40, lv_disp_get_ver_res(NULL) - 100);
-  lv_obj_align(chart, LV_ALIGN_CENTER, 0, 20);
+  lv_obj_set_size(chart, lv_obj_get_width(t3_content) - 50, lv_obj_get_height(t3_content) - 80);
+  lv_obj_align_to(chart, NULL, LV_ALIGN_CENTER, 0, 0);
   
   // Chart settings
   lv_chart_set_type(chart, LV_CHART_TYPE_LINE);
   lv_chart_set_point_count(chart, data.size());
-  lv_chart_set_range(chart, LV_CHART_AXIS_PRIMARY_Y, -10, 30);  // Temperature range -10 to 30°C
-  
+  if(selectedParam == "Temperature") {
+    lv_chart_set_range(chart, LV_CHART_AXIS_PRIMARY_Y, -10, 30);  // Temperature range -10 to 30°C
+  } else if(selectedParam == "Air_pressure") {
+    lv_chart_set_range(chart, LV_CHART_AXIS_PRIMARY_Y, 950, 1050); // Air pressure range 950 to 1050
+  } else if(selectedParam == "Humidity") {
+    lv_chart_set_range(chart, LV_CHART_AXIS_PRIMARY_Y, 0, 100); // Humidity range 0 to 100
+  } else if(selectedParam == "Wind_speed") {
+    lv_chart_set_range(chart, LV_CHART_AXIS_PRIMARY_Y, 0, 30); // Wind speed range 0 to 30
+  }
+
   // Style
   lv_obj_set_style_bg_opa(chart, LV_OPA_COVER, 0);
   lv_obj_set_style_bg_color(chart, lv_color_white(), 0);
@@ -213,6 +322,8 @@ static void create_forecast_table(const std::vector<int>& data)
   lv_obj_t* table = lv_table_create(t2_content);
   lv_table_set_row_cnt(table, DAYS + 1);   // +1 for header
   lv_table_set_col_cnt(table, 3);
+  lv_obj_set_size(table, lv_obj_get_width(t2_content) - 180, lv_obj_get_height(t2_content) - 60);
+  lv_obj_align_to(table, NULL, LV_ALIGN_CENTER, 0, 0);
 
   // Header
   lv_table_set_cell_value(table, 0, 0, "Day");
@@ -220,11 +331,14 @@ static void create_forecast_table(const std::vector<int>& data)
   if(selectedParam == "Temperature"){
     lv_table_set_cell_value(table, 0, 2, "Temperature");
   }
-  if(selectedParam == "Rain"){
-    lv_table_set_cell_value(table, 0, 2, "Rain");
+  if(selectedParam == "Air_pressure"){
+    lv_table_set_cell_value(table, 0, 2, "Air_pressure");
   }
   if(selectedParam == "Wind_speed"){
     lv_table_set_cell_value(table, 0, 2, "Wind_speed");
+  }
+  if(selectedParam == "Humidity"){
+    lv_table_set_cell_value(table, 0, 2, "Humidity");
   }
   lv_obj_align(table, LV_ALIGN_CENTER, 0, 0);
   // Fill rows with temperatures
@@ -245,13 +359,18 @@ static void create_forecast_table(const std::vector<int>& data)
       lv_table_set_cell_value(table, day + 1, 2, dataStr);
     }
 
-    if(selectedParam == "Rain"){
-      snprintf(dataStr, sizeof(dataStr), "%dmm", data[day]);
+    if(selectedParam == "Air_pressure"){
+      snprintf(dataStr, sizeof(dataStr), "%dhPa", data[day]);
       lv_table_set_cell_value(table, day + 1, 2, dataStr);
     }
 
     if(selectedParam == "Wind_speed"){
       snprintf(dataStr, sizeof(dataStr), "%dm/s", data[day]);
+      lv_table_set_cell_value(table, day + 1, 2, dataStr);
+    }
+
+    if(selectedParam == "Humidity"){
+      snprintf(dataStr, sizeof(dataStr), "%", data[day]);
       lv_table_set_cell_value(table, day + 1, 2, dataStr);
     }
   }
@@ -280,7 +399,7 @@ static void create_ui()
     lv_obj_set_style_text_font(t1_label, &lv_font_montserrat_32, 0);
     lv_obj_align(t1_label, LV_ALIGN_CENTER, 0, -20);
     lv_obj_t* text = lv_label_create(t1);
-    lv_label_set_text(text, "Version: 0.8");
+    lv_label_set_text(text, "Version: 2.7");
     lv_obj_set_style_text_font(text, &lv_font_montserrat_20, 0);
     lv_obj_align(text, LV_ALIGN_CENTER, 0, 10);
   }
@@ -328,20 +447,30 @@ static void create_ui()
     lv_obj_t* dd_city = lv_dropdown_create(t4);
     lv_dropdown_set_options(dd_city, "Karlskrona\n"
     "Stockholm\n"
-    "Goteborg\n");
+    "Goteborg\n"
+    "Malmo\n"
+    "Kiruna\n");
     lv_obj_align(dd_city, LV_ALIGN_TOP_MID, 0, 50);
     lv_obj_add_event_cb(dd_city, on_dd_city_clicked, LV_EVENT_VALUE_CHANGED, NULL);
+
+    // Show saved default city
+    lv_dropdown_set_text(dd_city, selectedCity.c_str());
     
     // Parameter
 
     lv_obj_t* dd_par = lv_dropdown_create(t4);
     lv_dropdown_set_options(dd_par, "Temperature\n"
-    "Rain\n"
-    "Wind_speed\n");
+    "Humidity\n"
+    "Wind_speed\n"
+    "Air_pressure\n");
     lv_obj_align(dd_par, LV_ALIGN_TOP_MID, 0, 150);
     lv_obj_add_event_cb(dd_par, on_dd_par_clicked, LV_EVENT_VALUE_CHANGED, NULL);
+
+    // Show saved default parameter
+
+    lv_dropdown_set_text(dd_par, selectedParam.c_str());
   }
-  
+
   lv_obj_scroll_to(tileview, 0, 0, LV_ANIM_OFF);
 }
 
@@ -381,29 +510,10 @@ void setup()
   create_ui();
   connect_wifi();
 
-  // Proceed to chart creation upon boot
-  std::vector<int> noonData = fetchNoonData(str, LAT, LON);
-  std::vector<int> pastData = fetchPastData(Average_AirTemp, 65090);
-  
-  printVector(pastData);
-  // Create and plot the chart
-  if (!pastData.empty()) {
-    create_temperature_chart(pastData);
-  } else {
-    lv_label_set_text(t3_label, "Failed to load historical data");
-    Serial.println("No temperature data received");
-    
-  }
-  
-  
-  if (!noonData.empty()) {
-    create_forecast_table(noonData);
-  } else {
-    lv_label_set_text(t2_label, "Failed to load forecast data");
-    Serial.println("No temperature data received");
-  }
-  
+  loadDefaults();
 
+  create_settings_button();
+  create_save_buttons();
 }
 
 // Must have function: Loop runs continously on device after setup
